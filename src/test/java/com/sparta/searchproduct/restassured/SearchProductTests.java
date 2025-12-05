3580f3f7309e20de4c37d97b64059ea298395120
parrais <github@parrais.co.uk>
2025-12-05 13:52:04 +0000

    Complete searchproduct tests.

--- src/test/java/com/sparta/searchproduct/restassured/SearchProductTests.java
+++ src/test/java/com/sparta/searchproduct/restassured/SearchProductTests.java
@@ -8,7 +8,6 @@ import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -56,8 +55,29 @@ public class SearchProductTests {
     }
 
     @Test
-    @DisplayName("POST To Search Product with parameter gives 200 OK")
+    @DisplayName("POST To Search Product with invalid parameter gives 400 bad request method")
     void postToSearchProductWithParameterGives200OK() {
+        response = RestAssured
+                .given()
+                .baseUri(BASE_URI)
+                .basePath("/api")
+                .headers("Accept", "application/json")
+                .contentType("multipart/form-data")
+                .multiPart("bad_request", "x")
+                .expect().defaultParser(Parser.JSON)
+                .when()
+                .post("/searchProduct")
+                .then()
+                .log().all(false)
+                .extract().response();
+        searchProductResponse = response.as(SearchProductResponse.class);
+        MatcherAssert.assertThat(searchProductResponse.getResponseCode(), Matchers.is(400));
+        MatcherAssert.assertThat(searchProductResponse.getMessage(), Matchers.is("Bad request, search_product parameter is missing in POST request."));
+    }
+
+    @Test
+    @DisplayName("POST To Search Product with parameter gives 200 OK")
+    void postToSearchProductWithInvalidParameterGives200OK() {
         response = RestAssured
                 .given()
                 .baseUri(BASE_URI)
@@ -94,5 +114,72 @@ public class SearchProductTests {
         searchProductResponse = response.as(SearchProductResponse.class);
         MatcherAssert.assertThat(searchProductResponse.getResponseCode(), Matchers.is(200));
         MatcherAssert.assertThat(searchProductResponse.getProducts().size(),Matchers.not(Matchers.is(0)));
+    }
+
+    @Test
+    @DisplayName("POST To Search Product with valid parameter and no results gives 200 OK and returns results")
+    void postToSearchProductWithValidParameterNoResultsGives200OK() {
+        response = RestAssured
+                .given()
+                .baseUri(BASE_URI)
+                .basePath("/api")
+                .headers("Accept", "application/json")
+                .contentType("multipart/form-data")
+                .multiPart("search_product", "gvsahgsfjsfgjzasgjmk")
+                .expect().defaultParser(Parser.JSON)
+                .when()
+                .post("/searchProduct")
+                .then()
+                .log().all(false)
+                .extract().response();
+        searchProductResponse = response.as(SearchProductResponse.class);
+        MatcherAssert.assertThat(searchProductResponse.getResponseCode(), Matchers.is(200));
+        MatcherAssert.assertThat(searchProductResponse.getProducts().size(),Matchers.is(0));
+    }
+
+    @Test
+    @DisplayName("POST To Search Product with valid results in name gives 200 OK and returns results")
+    void postToSearchProductWithValidNameMatchGives200OK() {
+        response = RestAssured
+                .given()
+                .baseUri(BASE_URI)
+                .basePath("/api")
+                .headers("Accept", "application/json")
+                .contentType("multipart/form-data")
+                .multiPart("search_product", "polo")
+                .expect().defaultParser(Parser.JSON)
+                .when()
+                .post("/searchProduct")
+                .then()
+                .log().all(false)
+                .extract().response();
+        searchProductResponse = response.as(SearchProductResponse.class);
+        MatcherAssert.assertThat(searchProductResponse.getResponseCode(), Matchers.is(200));
+        MatcherAssert.assertThat(searchProductResponse.getProducts().size(),Matchers.not(Matchers.is(0)));
+        MatcherAssert.assertThat(searchProductResponse.getProducts().getFirst().getName().toLowerCase(),Matchers.containsString("polo"));
+        MatcherAssert.assertThat(searchProductResponse.getProducts().getFirst().getCategory().getCategory().toLowerCase(),Matchers.not(Matchers.containsString("polo")));
+    }
+
+    @Test
+    @DisplayName("POST To Search Product with valid results in category gives 200 OK and returns results")
+    void postToSearchProductWithValidCategoryMatchGives200OK() {
+        response = RestAssured
+                .given()
+                .baseUri(BASE_URI)
+                .basePath("/api")
+                .headers("Accept", "application/json")
+                .contentType("multipart/form-data")
+                .multiPart("search_product", "tshirts")
+                .expect().defaultParser(Parser.JSON)
+                .when()
+                .post("/searchProduct")
+                .then()
+                .log().all(false)
+                .extract().response();
+        searchProductResponse = response.as(SearchProductResponse.class);
+        MatcherAssert.assertThat(searchProductResponse.getResponseCode(), Matchers.is(200));
+        MatcherAssert.assertThat(searchProductResponse.getProducts().size(),Matchers.not(Matchers.is(0)));
+        MatcherAssert.assertThat(searchProductResponse.getProducts().getFirst().getName().toLowerCase(),Matchers.not(Matchers.containsString("tshirts")));
+        MatcherAssert.assertThat(searchProductResponse.getProducts().getFirst().getCategory().getCategory().toLowerCase(),Matchers.containsString("tshirts"));
     }
 }
